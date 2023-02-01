f8a92bb84a868f2c3db3cabe2a9290355ca5b56d
jonas <jonas.peixoto.087@gmail.com>
2023-02-01 16:26:53 -0300

    Criando condiçoes de validação de cadastro pelo metodo Get

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import cadastro, login
+from .views import cadastro, login, valida_cadastro
 
 
 urlpatterns = [
     path('login/', login, name = 'login'),
     path('cadastro/', cadastro, name = 'cadastro'),
+    path('valida_cadastro/', valida_cadastro, name = 'valida_cadastro' )
    
 ]
